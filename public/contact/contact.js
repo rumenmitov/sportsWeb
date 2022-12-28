8fc94fbc7292127c40574bd7e71230c28ccef769
rumenmitov <rumen.valmitov@gmail.com>
2022-12-28 19:26:19 +0100

    Simplified way to report bugs throughout the entire website - Error response now come with a button that lets the user report the problem they are facing

--- public/contact/contact.js
+++ public/contact/contact.js
@@ -1,8 +1,24 @@
 let form = document.querySelector('form');
+let reportTypeSelect = document.querySelector('#type').children;
 
 screenCover.style.display = "none";
 loader.style.display = "none";
 coverText.style.display = "none";
+
+const bugType = location.href.split('?reportType=')[1];
+if (bugType === 'signup') {
+    reportTypeSelect[1].setAttribute("selected", 'true');
+} else if (bugType === 'results') {
+    reportTypeSelect[2].setAttribute("selected", 'true');
+} else if (bugType === 'teams') {
+    reportTypeSelect[3].setAttribute("selected", 'true');
+} else if (bugType === 'question') {
+    reportTypeSelect[4].setAttribute("selected", 'true');
+} else if (bugType === 'compliment') {
+    reportTypeSelect[5].setAttribute("selected", 'true');
+}  else if (bugType === 'other') {
+    reportTypeSelect[6].setAttribute("selected", 'true');
+}
 
 form.addEventListener('submit', ()=>{
     screenCover.style.display = "inline-block";
